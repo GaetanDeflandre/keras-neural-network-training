f25a09356f9a0ff9f00cd58fe2302c149a18b857
Gaetan <gaetan.deflandre@gmail.com>
2020-10-03 16:54:08 +0900

    Add code to train the model and show accuracy.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from keras.layers import Dense
 import numpy as np
 import matplotlib.pyplot as plt
 
+#%%
+# Load the data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 print(X_train.shape)
@@ -14,10 +16,13 @@ print(y_train.shape)
 print(X_test.shape)
 print(y_test.shape)
 
+#%%
 #plt.imshow(X_train[0], cmap='gray')
 #plt.show()
 #print(y_train[0])
 
+#%%
+# Preprocessing the image data
 image_height, image_width = 28, 28
 X_train = X_train.reshape(60000, image_height*image_width)
 X_test = X_test.reshape(10000, image_height*image_width)
@@ -25,6 +30,7 @@ X_test = X_test.reshape(10000, image_height*image_width)
 print(X_train.shape)
 print(X_test.shape)
 
+#%%
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 
@@ -36,7 +42,22 @@ y_test = to_categorical(y_test, 10)
 print(y_train.shape)
 print(y_test.shape)
 
+#%%
+# Build a model
 model = Sequential()
 model.add(Dense(512, activation='relu', input_shape=(784,)))
 model.add(Dense(512, activation='relu'))
 model.add(Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+# %%
+# Train the model
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_test, y_test))
+
+
+# %%
+# Show the accuracy
+plt.plot(history.history['acc'])
+plt.show()
